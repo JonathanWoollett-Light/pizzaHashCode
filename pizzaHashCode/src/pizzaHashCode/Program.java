1d7b4f392b7fb1b49c85a19e3924e48368832129
JonathanWoollett-Light <jonathanwoollettlight@gmail.com>
2018-02-10 19:02:59 +0000

    Added proper min size slice creation
    I've done quite a lot here and quite a bit of it is rather inefficient I
    feel we may need to discuss this so you guys can further add to this.

--- pizzaHashCode/src/pizzaHashCode/Program.java
+++ pizzaHashCode/src/pizzaHashCode/Program.java
@@ -26,57 +26,267 @@ public class Program {
 				if(pizzaSlices[y][x] == 0) {
 					if(createSlice(y, x, sliceCounter)) {
 						sliceCounter++;
-						
 					}
 				}
 			}
 		}
 	}
 	public static boolean createSlice(int originX, int originY, int sliceCounter) {
+		int firstMethodSize = verticalXSlice(originX, originY, sliceCounter);
+		if(firstMethodSize == minTomatoes + minMushrooms) {
+			return true;
+		}
+		revertSlice(sliceCounter + 1);
+		int minValue = firstMethodSize;
+		int minIndex = 0;
+		
+		int secondMethodSize = verticalYSlice(originX, originY, sliceCounter);
+		if(secondMethodSize == minTomatoes + minMushrooms) {
+			return true;
+		}
+		revertSlice(sliceCounter + 1);
+		
+		if(secondMethodSize < minValue) {
+			minValue = secondMethodSize;
+			minIndex = 1;
+		}
+		
+		int thirdMethodSize = horizontalXSlice(originX, originY, sliceCounter);
+		if(thirdMethodSize == minTomatoes + minMushrooms) {
+			return true;
+		}
+		revertSlice(sliceCounter + 1);
+		
+		if(thirdMethodSize < minValue) {
+			minValue = thirdMethodSize;
+			minIndex = 2;
+		}
+		
+		int fourthMethodSize = horizontalYSlice(originX, originY, sliceCounter);
+		if(fourthMethodSize == minTomatoes + minMushrooms) {
+			return true;
+		}
+		revertSlice(sliceCounter + 1);
+		if(fourthMethodSize < minValue) {
+			minValue = fourthMethodSize;
+			minIndex = 3;
+			
+		}
+		
+		if(minValue == Integer.MAX_VALUE) {
+			return false;
+		}
+		switch(minIndex) {
+		case 0:
+			verticalXSlice(originX, originY, sliceCounter);
+			break;
+		case 1:
+			verticalYSlice(originX, originY, sliceCounter);
+			break;
+		case 2:
+			horizontalXSlice(originX, originY, sliceCounter);
+			break;
+		case 3:
+			horizontalYSlice(originX, originY, sliceCounter);
+		}
+		return true;
+	}
+	
+	public static int verticalXSlice(int originX, int originY, int sliceCounter) {
 		int containedTomatoes = 0;
 		int containedMushrooms = 0;
-		
 		int firstMethodSize = 0;
-		firstMethod:
+		
+		for(int y = originY; y < pizzaYSize; y++) {
+			if(pizzaSlices[y][originX] != 0 || pizzaSlices[y][originX + 1] != 0 || firstMethodSize == maxSliceSize) {
+				revertSlice(true, originX, originY, y);
+				return Integer.MAX_VALUE;
+			}
+			
+			pizzaSlices[y][originX] = sliceCounter + 1;
+			pizzaSlices[y][originX + 1] = sliceCounter + 1;
+			
+			if(pizza[y][originX] == 'T') {
+				containedTomatoes++;
+			}else if(pizza[y][originX] == 'M') {
+				containedMushrooms++;
+			}
+			if(pizza[y][originX + 1] == 'T') {
+				containedTomatoes++;
+			}else if(pizza[y][originX + 1] == 'M') {
+				containedMushrooms++;
+			}
+			
+			firstMethodSize += 2;
+			if(containedTomatoes >= minTomatoes && containedMushrooms >= minMushrooms) {
+				return firstMethodSize;
+			}
+		}
+		return Integer.MAX_VALUE;//this is actually impossible to hit but Eclipse is complaining when this is not here
+	}
+	public static int verticalYSlice(int originX, int originY, int sliceCounter) {
+		int secondMethodSize = 0;
+		int containedTomatoes = 0;
+		int containedMushrooms = 0;
+		for(int y = originY; y < pizzaYSize; y++) {
+			if(pizzaSlices[y][originX] != 0 || secondMethodSize == maxSliceSize) {
+				revertSlice(true, originX, originY, false, y);
+				return Integer.MAX_VALUE;
+			}
+			
+			pizzaSlices[y][originX] = sliceCounter + 1;
+			
+			if(pizza[y][originX] == 'T') {
+				containedTomatoes++;
+			}else if(pizza[y][originX] == 'M') {
+				containedMushrooms++;
+			}
+			secondMethodSize++;
+			if(containedTomatoes >= minTomatoes && containedMushrooms >= minMushrooms) {
+				return secondMethodSize;
+			}
+		}
+		for(int y = originY; y < pizzaYSize; y++) {
+			if(pizzaSlices[y][originX + 1] != 0) {
+				revertSlice(true, originX, originY, true, y);
+				return Integer.MAX_VALUE;
+			}
+			
+			if(pizza[y][originX + 1] == 'T') {
+				containedTomatoes++;
+			}else if(pizza[y][originX + 1] == 'M') {
+				containedMushrooms++;
+			}
+			
+			pizzaSlices[y][originX + 1] = sliceCounter + 1;
+		}
+		secondMethodSize += pizzaYSize - originY;
+		if(!(containedTomatoes >= minTomatoes && containedMushrooms >= minMushrooms) || secondMethodSize > maxSliceSize) {
+			revertSlice(true, originX, originY, true, pizzaYSize - 1);
+			return Integer.MAX_VALUE;
+		}
+		return secondMethodSize;
+	}
+	public static int horizontalXSlice(int originX, int originY, int sliceCounter) {
+		int thirdMethodSize = 0;
+		int containedTomatoes = 0;
+		int containedMushrooms = 0;
+		for(int x = originX; x < pizzaXSize; x++) {
+			if(pizzaSlices[originY][x] != 0 || pizzaSlices[originY + 1][x] != 0 || thirdMethodSize == maxSliceSize) {
+				revertSlice(false, originX, originY, x);
+				return Integer.MAX_VALUE;
+			}
+			
+			pizzaSlices[originY][x] = sliceCounter + 1;
+			pizzaSlices[originY + 1][x] = sliceCounter + 1;
+			
+			if(pizza[originY][x] == 'T') {
+				containedTomatoes++;
+			}else if(pizza[originY][x] == 'M') {
+				containedMushrooms++;
+			}
+			if(pizza[originY + 1][x] == 'T') {
+				containedTomatoes++;
+			}else if(pizza[originY + 1][x] == 'M') {
+				containedMushrooms++;
+			}
+			
+			thirdMethodSize += 2;
+			if(containedTomatoes >= minTomatoes && containedMushrooms >= minMushrooms) {
+				return thirdMethodSize;
+			}
+		}
+		return Integer.MAX_VALUE;//this is actually impossible to hit but Eclipse is complaining when this is not here
+	}
+	public static int horizontalYSlice(int originX, int originY, int sliceCounter) {
+		int fourthMethodSize = 0;
+		int containedTomatoes = 0;
+		int containedMushrooms = 0;
+		for(int x = originX; x < pizzaXSize; x++) {
+			if(pizzaSlices[originY][x] != 0 || fourthMethodSize == maxSliceSize) {
+				revertSlice(false, originX, originY, false, x);
+				return Integer.MAX_VALUE;
+			}
+			
+			pizzaSlices[originY][x] = sliceCounter + 1;
+			
+			if(pizza[originY][x] == 'T') {
+				containedTomatoes++;
+			}else if(pizza[originY][x] == 'M') {
+				containedMushrooms++;
+			}
+			fourthMethodSize++;
+			if(containedTomatoes >= minTomatoes && containedMushrooms >= minMushrooms) {
+				return fourthMethodSize;
+			}
+		}
+		for(int x = originX; x < pizzaXSize; x++) {
+			if(pizzaSlices[originY + 1][x] != 0) {
+				revertSlice(false, originX, originY, true, x);
+				return Integer.MAX_VALUE;
+			}
+			
+			if(pizza[originY + 1][x] == 'T') {
+				containedTomatoes++;
+			}else if(pizza[originY + 1][x] == 'M') {
+				containedMushrooms++;
+			}
+			
+			pizzaSlices[originY + 1][x] = sliceCounter + 1;
+		}
+		fourthMethodSize += pizzaXSize - originX;
+		if(!(containedTomatoes >= minTomatoes && containedMushrooms >= minMushrooms) || fourthMethodSize > maxSliceSize) {
+			revertSlice(false, originX, originY, true, pizzaXSize - 1);
+			return Integer.MAX_VALUE;
+		}
+		return fourthMethodSize;
+	}
+	
+	
+	public static void revertSlice(int sliceCounter) {
 		for(int y = 0; y < pizzaYSize; y++) {
-			for(int x = 0; x < 2; x++) {
-				
-				if(pizzaSlices[y][x] != 0) {
-					revertSlice(1, originX, originY, x, y);
-					firstMethodSize = -1;
-					break firstMethod;
-				}
-				
-				pizzaSlices[y][x] = sliceCounter + 1;
-				switch(pizza[y][x]) {
-				case 'T':
-					containedTomatoes++;
-					break;
-				case 'M':
-					containedMushrooms++;
-				}
-				firstMethodSize++;
-				if(containedTomatoes > minTomatoes && containedMushrooms > minMushrooms) {
-					break firstMethod;
+			for(int x = 0; x < pizzaYSize; x++) {
+				if(pizza[y][x] == sliceCounter) {
+					pizza[y][x] = 0;
 				}
 			}
 		}
 	}
-	public static void revertSlice(int method, int originX, int originY, int lastX, int lastY) {
-		if(method == 1) {
-			for(int y = lastY; y > originY; y--) {
-				for(int x = lastX; x > originX - 1; x--) {
-					pizzaSlices[y][x] = 0;
+	public static void revertSlice(boolean method, int originX, int originY, int lastValue) {
+		if(method) {
+			for(int y = lastValue; y >= originY; y--) {
+				pizzaSlices[y][originX] = 0;
+				pizzaSlices[y][originX + 1] = 0;
+			}
+		}else {
+			for(int x = lastValue; x >= originX; x--) {
+				pizzaSlices[originY][x] = 0;
+				pizzaSlices[originY + 1][x] = 0;
+			}
+		}
+		
+	}
+	public static void revertSlice(boolean method, int originX, int originY, boolean twoColumns, int lastValue) {
+		if(method) {
+			for(int y = lastValue; y >= originY; y--) {
+				pizzaSlices[y][originX] = 0;
+			}
+			if(twoColumns) {
+				for(int y = lastValue; y >= originY; y--) {
+					pizzaSlices[y][originX + 1] = 0;
 				}
 			}
-		}else if(method == 2) {
-			
-			for(int y = lastY; y > originY; y--) {
-				for(int x = lastX; x > originX - 1; x--) {
-					pizzaSlices[y][x] = 0;
+		}else {
+			for(int x = lastValue; x >= originX; x--) {
+				pizzaSlices[originY][x] = 0;
+			}
+			if(twoColumns) {
+				for(int x = lastValue; x >= originX; x--) {
+					pizzaSlices[originY + 1][x] = 0;
 				}
 			}
 		}
+		
 	}
 	public static void makePizza() {
 		Random rand = new Random();
